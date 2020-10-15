c1b0e511e1956c3ec982d3a0b59e84bd2ec0884b
MicheaBoab <50056681+MicheaBoab@users.noreply.github.com>
2020-10-15 00:02:18 -0500

    Add files via upload

--- st.h
+++ st.h
@@ -1,7 +1,8 @@
-#ifndef ST_H
+﻿#ifndef ST_H
 #define ST_H
 
 #include <QDialog>
+#include <st_dialog.h>
 
 namespace Ui {
 class ST;
@@ -15,8 +16,31 @@ public:
     explicit ST(QWidget *parent = nullptr);
     ~ST();
 
+public slots:
+    void on_taskEnterButton_clicked();
+
+    void on_taskRemoveButton_clicked();
+
+private slots:
+    void on_saveButton_clicked();
+
+    void on_loadButton_clicked();
+
 private:
     Ui::ST *ui;
-};
 
+    QString name;
+    QString wcet, deadline, period, jitter, offset;
+    QString isperiodic;
+
+    enum Column
+    {
+        Name, WCET, Deadline, Period, Jitter, Offset, Periodic_or_sporadic,
+    };
+
+    QVector<QString> m_data;
+
+    int item_counter = 0;
+    int id = 1;
+};
 #endif // ST_H
